b460750e252199257dca73d6898c507ee388b4d4
zaka <zakaria.harsoyo@gmail.com>
2024-11-06 00:23:18 +0700

    fix: rearrange and remove some coloumns
    remove the 'id' column from alternativeValues table and rearrange the criteria table

--- database/migrations/2024_11_03_141345_create_alternativeValues_table.php
+++ database/migrations/2024_11_03_141345_create_alternativeValues_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('alternativeValues', function (Blueprint $table) {
-            $table->id();
             $table->foreignId('alternative_id')->constrained()->onDelete('cascade');
             $table->foreignId('criteria_id')->constrained()->onDelete('cascade');
             $table->decimal('score', 5, 2);
